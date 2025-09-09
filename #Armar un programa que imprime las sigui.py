46b2d756e8f24f4dda5bb0e09a3e38c4bdb48834
TomyGrace <gracevichtomas@gmail.com>
2025-09-09 20:42:32 -0300

    Mi primer commit

---
+++ #Armar un programa que imprime las sigui.py
@@ -0,0 +1,13 @@
+#Armar un programa que imprime las siguientes listas
+lista = [
+    [121,"Perez",  10],  
+
+    [121,"Lopez", 100],
+
+    [404, "Ronald" , 50],
+
+    [333, "Gomez", 50],
+
+    [ 333, "Dahan", 20 ],
+]
+print(lista)
