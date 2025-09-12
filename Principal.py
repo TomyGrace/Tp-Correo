93383e8f5541953786444e45acff0a699bdc1dd9
TomyGrace <gracevichtomas@gmail.com>
2025-09-12 19:24:09 -0300

    se-declararon-clases-y-algunos-atributos

--- Principal.py
+++ Principal.py
@@ -1,5 +1,25 @@
+class ServidorCorreo:
+    def __init__ (self, nombre):
+        self.nombre=nombre
+        self.__usuarios=
+        self.__registros=
+
 class Usuario:
     def __init__ (self, nombre, correo, contraseña):
         self.nombre=nombre
         self.correo=correo
         self.__contraseña=contraseña
+
+class Carpeta:
+    def __init__(self, propietario, nombre, mensaje):
+        self.propietario=propietario
+        self.nombre=nombre
+        self.mensaje=mensaje
+
+class Mensaje:
+    def __init__(self, destinatario, remitente, asunto, cuerpo, id):
+        self.destinatario=destinatario
+        self.remitente=remitente
+        self.asunto=asunto
+        self.cuerpo=cuerpo
+        self.__id=id
