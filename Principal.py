9736a378a58fef1d3011bf7864a0b272625bb48f
TomyGrace <gracevichtomas@gmail.com>
2025-09-16 23:42:49 -0300

    se agregaron metodos para agregar y mostrar carpetas

--- Principal.py
+++ Principal.py
@@ -10,7 +10,6 @@ class carpeta_abstracta (ABC):
     @abstractmethod
     def ListarMensajes(self):
         pass
-    
 
 
 
@@ -23,6 +22,9 @@ class ServidorCorreo(carpeta_abstracta):
     def _MostrarUsuario (self):
         for usuario in self.__usuarios.values():
             print(usuario.devolver_info())
+    def _MostrarCarpeta(self):
+        for usuario in self.__usuarios.values():
+            print(usuario.devolver_carpetas())
     def EnviarMensajes(self):
         pass
     def RecibirMensajes(self):
@@ -30,22 +32,42 @@ class ServidorCorreo(carpeta_abstracta):
     def ListarMensajes(self):
         pass
 
-class Usuario:
+class Usuario(carpeta_abstracta):
     def __init__ (self, nombre, contraseña):
         self.nombre=nombre
         self.__contraseña=contraseña
+        self.__carpetas=[
+
+        ]
     def __get_contraseña(self):
         return self.__contraseña
     def __informacion(self):
         return f"Usuario({self.nombre})"
+    def __get_carpeta(self):
+        return self.__carpetas
     def devolver_info(self):
         return self.__informacion()
+    def devolver_carpetas(self):
+        return self.__get_carpeta()
+    def EnviarMensajes(self):
+        pass
+    def RecibirMensajes(self):
+        pass
+    def ListarMensajes(self):
+        pass
+    def CrearCarpeta(self, Nombre):
+        self.__carpetas.append(
+            Nombre=Carpeta("self.nombre", f"{Nombre}")
+            )
+        
 
 class Carpeta(carpeta_abstracta):
-    def __init__(self, propietario, nombre, mensaje):
+    def __init__(self, propietario, nombre):
         self.propietario=propietario
         self.nombre=nombre
-        self.mensaje=mensaje
+        self.mensaje={
+
+        }
     def EnviarMensajes(self):
         pass
     def RecibirMensajes(self):
@@ -59,7 +81,8 @@ class Mensaje:
         self.remitente=remitente
         self.asunto=asunto
         self.cuerpo=cuerpo
-
+    
 
 servidor=ServidorCorreo()
 servidor._MostrarUsuario()
+servidor._MostrarCarpeta()
